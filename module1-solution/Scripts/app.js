aa45ebf1df744c91421f0e18ac228085fc12bf6b
mallinookala03 <malli.nookala@hotmail.com>
2017-11-28 17:01:39 -0800

    script changes

--- module1-solution/Scripts/app.js
+++ module1-solution/Scripts/app.js
@@ -1,26 +1,25 @@
-angular.module('LunchCheck', [])
-.controller("LunchCheckController", LunchCheckController);
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
-$scope.items="";
-$scope.message="";
-function CheckItems()
-{
-  if($scope.items=="")
-  { 
-  $scope.message="Please enter data first";
-  }
-  else
-  {
-    var items[] = $scope.items.split(",");
-    if(items.length<=3)
-    {
-     $scope.message="Enjoy!";
-    }
-    else
-    {
-    $scope.message="Too much!";
-    }
-  }
-};
-}
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+    .controller("LunchCheckController", LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.items = "";
+        $scope.message = "";
+        $scope.CheckItems = function() {
+            if ($scope.items == "") {
+                $scope.message = "Please enter data first";
+            }
+            else {
+                var selitems = $scope.items.split(",");
+                if (selitems.length <= 3) {
+                    $scope.message = "Enjoy!";
+                }
+                else {
+                    $scope.message = "Too much!";
+                }
+            }
+        }
+    };
+})();
